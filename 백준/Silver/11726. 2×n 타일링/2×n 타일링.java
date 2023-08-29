b301c3e8b0a9bbfef2294ca1f516cbb69426f53f
tngur1101 <tngur1101@naver.com>
2023-08-29 14:52:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 80 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * 2xn 크기의 직사각형을 1x2, 2x1 타일로 채우는 방법의 수를 구하시오
+ * @author SSAFY
+ *
+ */
+
+public class Main {
+	
+	static int N;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		long dp[] = new long[1001];
+		dp[1] = 1;
+		dp[2] = 2;
+		
+		for(int i=3;i<=N;i++) {
+			dp[i] = dp[i-1]%10007+dp[i-2]%10007;
+		}
+		
+		System.out.println(dp[N]%10007);
+	}
+
+}
